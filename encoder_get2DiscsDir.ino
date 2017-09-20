04eb2d2c09d4dd3a654049b510f2e82348e6dc02
Tomohiro <yokota.0529@gmail.com>
2017-09-20 23:07:15 +0900

    update

--- encoder_get2DiscsDir.ino
+++ encoder_get2DiscsDir.ino
@@ -1,53 +1,59 @@
-// get direction of the rotating disc by encoder  
+// get direction of the rotating disc by encoder
 
-int pin_enc_1 = 5;
+int pin_enc_1 = 6;
 int pin_enc_2 = 7;
-# define PIN_ENC_1 5
+# define PIN_ENC_1 6
 # define PIN_ENC_2 7
 # define PIN_BUSY_1 9
 # define PIN_BUSY_2 10
-# define PIN_LED 13
+# define PIN_LED_1 12
+# define PIN_LED_2 13
 
 boolean initBool;
 
-const int numSlit1 = 6;
-const int numSlit2 = 8;
+const int initRot = 5; // the amount of init rotation for calibrating encoder.
 
-double slitThd1;
-double slitThd2;
-
-struct encState{
+struct encoderValues{
+        int numSlit;
         int slitCount;
+        double slitThd;
         boolean encVal_cr;
         boolean encVal_prv;
-        unsigned long falseSrt;        
-        unsigned long falseEnd;                   
+        unsigned long falseSrt;
+        unsigned long falseEnd;
+        double falseNrrwAve;
+        double falseWidAve;
 };
 
-struct encState enc1;
-struct encState enc2;
+struct encoderValues enc1;
+struct encoderValues enc2;
 
 void setup() {
         Serial.begin(2000000);
         pinMode(PIN_ENC_1, INPUT);
         pinMode(PIN_ENC_2, INPUT);
-        pinMode(PIN_LED, OUTPUT);
+        pinMode(PIN_LED_1, OUTPUT);
+        pinMode(PIN_LED_2, OUTPUT);
 
-        initBool = false;
-        
+        initBool = true;
+
+        enc1.numSlit = 6;
+        enc2.numSlit = 8;
+
         enc1.slitCount = 0;
         enc2.slitCount = 0;
         enc1.falseSrt = micros();
         enc2.falseSrt = micros();
         enc1.encVal_prv = digitalRead(PIN_ENC_1);
         enc2.encVal_prv = digitalRead(PIN_ENC_2);
-        digitalWrite(PIN_LED, LOW);
+        digitalWrite(PIN_LED_1, LOW);
+        digitalWrite(PIN_LED_2, LOW);
 }
 
 void loop() {
         // get a BUSY flag from a motor driver.
+        // now, we use a stepping motor driven by L6470 and be waiting end of the acceleration.
         while( !( digitalRead(PIN_BUSY_1) && digitalRead(PIN_BUSY_2) ) ){ 
-               // now, we use a stepping motor driven by L6470 and be waiting end of the acceleration.
                initBool = true;
                Serial.println("busy_now");
                delay(100);
@@ -55,8 +61,8 @@ void loop() {
 
         // get threshold of the magnification between wide slit and thin slit.
         if( initBool == true ){
-                slitThd1 = getSlitThreshold( pin_enc_1, numSlit1 );
-                slitThd2 = getSlitThreshold( pin_enc_2, numSlit2 );
+                enc1.slitThd = getSlitThreshold( pin_enc_1, &enc1 ,initRot );
+                enc2.slitThd = getSlitThreshold( pin_enc_2, &enc2 ,initRot );
                 initBool = false;                
         }
         
@@ -66,8 +72,8 @@ void loop() {
                 enc1.encVal_cr = digitalRead(PIN_ENC_1); // in the slit area = false,  except area = true;
                 enc2.encVal_cr = digitalRead(PIN_ENC_2); // in the slit area = false,  except area = true;
                 
-                getEncState( &enc1, slitThd1 );
-                getEncState( &enc2, slitThd2 );
+                getEncState( &enc1 );
+                getEncState( &enc2 );
                 
                 enc1.encVal_prv = enc1.encVal_cr;
                 enc2.encVal_prv = enc2.encVal_cr;
--- encoder_get2DiscsDir_func.ino
+++ encoder_get2DiscsDir_func.ino
@@ -1,47 +1,37 @@
-double getSlitThreshold( const int pin_enc, const int numSlit ){
+double getSlitThreshold( const int pin_enc, struct encoderValues *enc , const int initRot){
         Serial.println("get threshold of the wide slit and narrow slit.");
-        boolean encVal_cr;
-        boolean encVal_prv;
-        unsigned int slitCount;
         unsigned int elemCount;
-        const int initRot = 3;
-        const int numElem =  ( numSlit + 1 ) * initRot;
-        unsigned long falseSrt;
-        unsigned long falseEnd;
-        unsigned long falseTime;
+        const int numElem =  ( enc->numSlit + 1 ) * initRot;
         unsigned long falseNrrw[ numElem ];
         unsigned long falseWid[ numElem ];
         double falseNrrwStd;
         double falseWidStd;
-        double falseNrrwAve;
-        double falseWidAve;
-        double slitThd;
-        
-        slitCount = 0;
+       
+        enc->slitCount = 0;
         elemCount = 0;
         for( int i = 0; i < numElem; i++ ){
                 falseNrrw[ i ] = 0;
                 falseWid[ i ] = 0;
         }
         
-        falseSrt = micros();
-        while( slitCount <= numElem + (numSlit+1) ){
-                encVal_cr = digitalRead(pin_enc); // in the slit area = false,  except area = true;
-                if (encVal_cr != encVal_prv){
-                        if( encVal_cr ==  false){ // start slit area
-                                falseSrt = micros();
+        enc->falseSrt = micros();
+        while( enc->slitCount <= numElem + (enc->numSlit+1) ){
+                enc->encVal_cr = digitalRead(pin_enc); // in the slit area = false,  except area = true;
+                if (enc->encVal_cr != enc->encVal_prv){
+                        if( enc->encVal_cr ==  false){ // start slit area
+                                enc->falseSrt = micros();
                         }
-                        if( encVal_cr == true ){ // end slit area
-                                falseEnd = micros();
-                                falseTime = falseEnd - falseSrt;
+                        if( enc->encVal_cr == true ){ // end slit area
+                                enc->falseEnd = micros();
+                                unsigned long falseTime = enc->falseEnd - enc->falseSrt;
                                 Serial.print( "false: " ); Serial.println( falseTime ); 
-                                slitCount += 1;
-                                if( slitCount <= numSlit+1 ){
+                                enc->slitCount += 1;
+                                if( enc->slitCount <= enc->numSlit+1 ){
                                         // except first rotation 
-                                }else if( slitCount == 1 + (numSlit+1) ){
+                                }else if( enc->slitCount == 1 + (enc->numSlit+1) ){
                                         falseNrrw[0] = falseTime;
                                         falseWid[0] = falseTime;
-                                }else if( slitCount <= numElem + (numSlit+1) ){
+                                }else if( enc->slitCount <= numElem + (enc->numSlit+1) ){
                                         elemCount  += 1;
                                         if ( falseTime > falseWid[ elemCount -1 ] * 0.8){
                                                 falseWid[ elemCount ] = falseTime;
@@ -51,7 +41,7 @@ double getSlitThreshold( const int pin_enc, const int numSlit ){
                                                 falseNrrw[ elemCount ] = falseTime;                                                        
                                                 falseWid[ elemCount ] = falseWid[ elemCount - 1 ];        
                                         }
-                                }else if( slitCount > numElem + (numSlit+1) ){
+                                }else if( enc->slitCount > numElem + (enc->numSlit+1) ){
                                         Serial.println("");
                                         Serial.println( "falseNrrw: "  );
                                         for( int i = 0; i < numElem; i++ ){
@@ -65,24 +55,25 @@ double getSlitThreshold( const int pin_enc, const int numSlit ){
                                         }
                                         falseNrrwStd = stdev( falseNrrw, numElem ); // standard deviation
                                         falseWidStd = stdev( falseWid, numElem ); // standard deviation
-                                        falseNrrwAve = exoutMean( falseNrrw, falseNrrwStd, numElem ); // mean except outliers
-                                        falseWidAve = exoutMean( falseWid, falseWidStd, numElem ); // mean except outliers
-                                        slitThd = sqrt( falseWidAve / falseNrrwAve );
+                                        enc->falseNrrwAve = exoutMean( falseNrrw, falseNrrwStd, numElem ); // mean except outliers
+                                        enc->falseWidAve = exoutMean( falseWid, falseWidStd, numElem ); // mean except outliers
+                                        enc->slitThd = sqrt( enc->falseWidAve / enc->falseNrrwAve );
                                         Serial.println("falseNrrwStd: "); Serial.println( falseNrrwStd );
-                                        Serial.println("falseNrrwAve: "); Serial.println( falseNrrwAve );
+                                        Serial.println("falseNrrwAve: "); Serial.println( enc->falseNrrwAve );
                                         Serial.println("falseWidStd: "); Serial.println( falseWidStd );
-                                        Serial.println("falseWidAve: "); Serial.println( falseWidAve );
-                                        Serial.println("threshold of magnification between falseWid and falseNrrw: "); Serial.println( slitThd );
+                                        Serial.println("falseWidAve: "); Serial.println( enc->falseWidAve );
+                                        Serial.println("threshold of magnification between falseWid and falseNrrw: "); Serial.println( enc->slitThd );
                                         Serial.println("");
                                 }
                         }
                 }
-                encVal_prv = encVal_cr;
+                enc->encVal_prv = enc->encVal_cr;
         }
-        return slitThd = falseWidAve / slitThd;
+        enc->slitCount = 0;
+        return enc->slitThd = enc->falseWidAve / enc->slitThd;
 }
 
-int getEncState( struct encState *enc, const double slitThd ){
+void getEncState( struct encoderValues *enc ){
         if( enc->encVal_cr != enc->encVal_prv ){
                 if ( enc->encVal_cr == false ){
                         enc->falseSrt = micros();
@@ -91,14 +82,13 @@ int getEncState( struct encState *enc, const double slitThd ){
                         enc->falseEnd = micros();
                         unsigned long falseTime = enc->falseEnd - enc->falseSrt;
                         //Serial.println(falseTime);
-                        if( falseTime  > slitThd ){
+                        if( falseTime  > enc->slitThd ){
                                 enc->slitCount = 0;
                         }else{
                                 enc->slitCount += 1;
                         }
                 }
         }
-        return 0;
 }
 
 double mean( unsigned long x[], int xlength ){
